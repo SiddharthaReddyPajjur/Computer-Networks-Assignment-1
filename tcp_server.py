f20383bf633b9ef0af4411db7b38ea8540b2da3d
SiddharthaReddyPajjur <145243311+SiddharthaReddyPajjur@users.noreply.github.com>
2025-08-29 14:38:58 +0530

    Add files via upload

---
+++ tcp_server.py
@@ -0,0 +1,22 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('127.0.0.1', 9999))
+server_socket.listen()
+
+print("🔵 Server is listening on port 9999...")
+
+conn, addr = server_socket.accept()
+print(f"✅ Connected by {addr}")
+
+while True:
+    data = conn.recv(1024).decode()
+    if not data or data.lower() == 'exit':
+        print("❌ Client disconnected.")
+        break
+
+    print(f"Client: {data}")
+    reply = input("Server: ")
+    conn.send(reply.encode())
+
+conn.close()
